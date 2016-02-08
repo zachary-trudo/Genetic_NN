7c4ca68d6af6e9a4b0aaf33f79cfee3d9e5c90f4
lockout87 <lockout87@gmail.com>
2016-02-08 15:49:09 -0800

    Moved a bunch of stuff from neuralNet.h to neuralNet.c

--- neuralNet.h
+++ neuralNet.h
@@ -11,78 +11,16 @@ typedef struct NodeTag
 } Node;
 
 
-Node* NodeCon(int numConnects, int** weights)
-{
-    Node *newNode;
-    newNode = (Node *) malloc(sizeof(Node));
-    setNodeConnects(newNode, numConnects);
-    setNodeWeights(newNode, weights);
-    return newNode;
-}
+Node* NodeCon(int numConnects, int** weights);
+Node* NodeRandCon(int numConnects);
 
+void deconNode(Node* node);
 
-Node* NodeRandCon(int numConnects)
-{
-    Node *newNode;
-    newNode = (Node *) malloc(sizeof(Node));
-    int** weights = getRandWeights(numConnects);
-    
-    setNodeConnects(newNode, numConnects);
-    setNodeWeights(newNode, weights);
-    return newNode;
-}
-
-void deconNode(Node* node)
-{
-    int i = 0;
-    for(i = 0; i < node->numConnects; i++)
-    {
-        free(node->weights[i]);
-    }
-    free(node);
-}
-
-
-int** getRandWeights(int numConnects)
-{
-    int i = 0;
-    int** weights;
-    weights = (int**) malloc(sizeof(int*) * numConnects);
-
-    for (i = 0; i < numConnects; i++)
-    {
-        weights[i] = (int*) malloc(sizeof(int));
-        weights[i] = tanh(GetRand(100));
-    }
-    return weights;
-}
-
-void setNodeConnects(Node* node, int numConnects)
-{
-    node->numConnects = numConnects;
-}
-
-void setNodeWeights(Node* node, int** weights)
-{
-    int i = 0;
-    node->weights = (int**) malloc(sizeof(int*) * node->numConnects);
-
-    for(i = 0; i < node->numConnects; i++)
-    {
-        node->weights[i] = (int*) malloc(sizeof(int));
-        node->weights[i] = weights[i];
-    }
-}
-
-int getNodeConnects(Node* node)
-{
-    return node->numConnects;
-}
-
-int** getNodeWeights(Node* node)
-{
-    return node->weights;
-}
+int** getRandWeights(int numConnects);
+void setNodeConnects(Node* node, int numConnects);
+void setNodeWeights(Node* node, int** weights);
+int getNodeConnects(Node* node);
+int** getNodeWeights(Node* node);
 
 typedef struct LayerTag
 {
@@ -91,64 +29,13 @@ typedef struct LayerTag
     int nextLayerNodes;
 } Layer;
 
-Layer* LayerCon(Node** nodes, int numNodes, int nextLayerNodes)
-{
-    Layer* layer;
-    layer = (Layer *) malloc(sizeof(Layer));
-    
-    setLayerNumNodes(layer, numNodes);
-    setLayerNodes(layer, nodes);
-    setLayerNextNodes(layer, nextLayerNodes);
-    return layer;
-}
+Layer* LayerCon(Node** nodes, int numNodes, int nextLayerNodes);
+Layer* LayerRandCon(int numNodes, int nextLayerNodes);
 
-Layer* LayerRandCon(int numNodes, int nextLayerNodes)
-{
-    int i = 0;
-    Layer* layer;
-    layer = (Layer *) malloc(sizeof(Layer));
-    setLayerNumNodes(layer, numNodes);
-    setLayerNextNodes(layer, nextLayerNodes);
-    
-    Node** nodes = (Node**) malloc(sizeof(Node*) * layer->numNodes);
+void setLayerNumNodes(Layer* layer, int numNodes);
+void setLayerNextNodes(Layer* layer, int nextLayerNodes);
 
-    for (i = 0; i < layer->numNodes; i++)
-    {
-        nodes[i] = NodeRandCon(layer->nextLayerNodes);
-    }
-
-    setLayerNodes(layer, nodes);
-    
-    for( i = 0; i < layer->numNodes; i++)
-    {
-        deconNode(nodes[i]);
-    }
-    free(nodes);
-
-    return layer;
-}
-
-void setLayerNumNodes(Layer* layer, int numNodes)
-{
-    layer->numNodes = numNodes;
-}
-
-void setLayerNextNodes(Layer* layer, int nextLayerNodes)
-{
-    layer->nextLayerNodes = nextLayerNodes;
-}
-
-void setLayerNodes(Layer* layer, Node** nodes)
-{
-    int i = 0;
-    layer->nodes = (Node**) malloc(sizeof(Node*) * layer->numNodes);
-
-    for(i = 0; i < layer->numNodes; i++)
-    {
-        layer->nodes[i] = NodeCon(nodes[i]->numConnects, nodes[i]->weights);
-    }
-}
-
+void setLayerNodes(Layer* layer, Node** nodes);
 
 typedef struct NetTag
 {
