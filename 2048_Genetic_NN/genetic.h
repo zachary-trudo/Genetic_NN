8c898080273a0f4574a937252e67fe228400cc7d
Zachary Trudo <ztrudo@uoregon.edu>
2016-04-17 12:45:16 -0700

    Clean up issues caused by move from bitbucket

--- 2048_Genetic_NN/genetic.h
+++
@@ -1,7 +0,0 @@
-#include "neuralNet.h"
-
-// This is where we choose the net for mating. 
-Net* ChooseNet(Net* nets[], int numNets, int avgScore, int avgMovei, int avgHeight);
-
-// Function for mating the weights.
-void MateWeights(Net* netOne, Net* netTwo);
